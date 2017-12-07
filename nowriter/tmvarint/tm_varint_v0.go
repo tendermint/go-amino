8b66e7eb628bb87a6e4854a89c2cfd57dca19cf5
Petabyte Storage <petabytestorage@gmail.com>
2017-12-07 10:55:03 -0800

    Add length-prefixed and unlength-prefixed array encoders to complete the set

--- nowriter/tmvarint/tm_varint_v0.go
+++ nowriter/tmvarint/tm_varint_v0.go
@@ -1,8 +1,6 @@
 package tmvarint
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/binary"
 	"github.com/tendermint/go-wire/nowriter/tmlegacy"
 )
@@ -14,51 +12,38 @@ var _ TMVarint = (*TMVarintV0)(nil)
 var legacy = tmlegacy.TMEncoderLegacy{}
 
 func (e TMVarintV0) EncodeUvarint(i uint) []byte {
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	var inst_n int
-	n := &inst_n
-	var inst_err error
-	err := &inst_err
-
 	var size = uvarintSize(uint64(i))
-	legacy.WriteUint8(uint8(size), w, n, err)
+	result := []byte{uint8(size)}
 	if size > 0 {
 		var buf [8]byte
 		binary.BigEndian.PutUint64(buf[:], uint64(i))
-		legacy.WriteTo(buf[(8-size):], w, n, err)
+		result = append(result, buf[(8-size):]...)
 	}
 
-	return b.Bytes()
+	return result
 }
 
 func (e TMVarintV0) EncodeVarint(i int) []byte {
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	var inst_n int
-	n := &inst_n
-	var inst_err error
-	err := &inst_err
-
 	var negate = false
 	if i < 0 {
 		negate = true
 		i = -i
 	}
 	var size = uvarintSize(uint64(i))
+	result := []byte{0}
 	if negate {
 		// e.g. 0xF1 for a single negative byte
-		legacy.WriteUint8(uint8(size+0xF0), w, n, err)
+		result[0] = uint8(size + 0xF0)
 	} else {
-		legacy.WriteUint8(uint8(size), w, n, err)
+		result[0] = uint8(size)
 	}
 	if size > 0 {
 		var buf [8]byte
 		binary.BigEndian.PutUint64(buf[:], uint64(i))
-		legacy.WriteTo(buf[(8-size):], w, n, err)
+		result = append(result, buf[(8-size):]...)
 	}
 
-	return b.Bytes()
+	return result
 }
 
 func uvarintSize(i uint64) int {
