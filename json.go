e83bd67a7fa1062e06b87930c87a91c5b9d2227d
Emmanuel Odeke <emm.odeke@gmail.com>
2018-01-16 02:06:22 -0700

    UnmarshalJSON decodeJSONInterface started
    Started with decodeJSONInterface

--- json.go
+++ json.go
@@ -2,6 +2,7 @@ package wire
 
 import (
 	"bytes"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,17 +15,23 @@ import (
 //----------------------------------------
 // cdc.encodeReflectJSON
 
+const (
+	disfixKeyQuoted = `"_df"`
+	dataKeyQuoted   = `"_v"`
+)
+
 func (cdc *Codec) encodeReflectJSON(w io.Writer, info *TypeInfo, rv reflect.Value, opts FieldOptions) (err error) {
-	if info.Registered {
+	disambiguate := info.Registered
+	if disambiguate {
 		// Write the disfix
 		disfix := toDisfix(info.Disamb, info.Prefix)
-		fmt.Fprintf(w, `{"_df":"%X","_v":`, disfix)
+		fmt.Fprintf(w, `{%s:"%X",%s:`, disfixKeyQuoted, disfix, dataKeyQuoted)
 	}
 
 	if err = cdc._encodeReflectJSON(w, info, rv, opts); err != nil {
 		return
 	}
-	if info.Registered {
+	if disambiguate {
 		_, err = w.Write(bytesCloseBrace)
 	}
 	return err
@@ -148,9 +155,6 @@ func (cdc *Codec) encodeReflectJSONInterface(w io.Writer, info *TypeInfo, rv ref
 	return cdc.encodeReflectJSON(w, cinfo, crv, opts)
 }
 
-func isPrimitiveGoKind(kind reflect.Kind) bool {
-}
-
 func (cdc *Codec) encodeReflectJSONStruct(w io.Writer, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
 	typ := rv.Type()
 	nf := typ.NumField()
@@ -208,7 +212,7 @@ func isExported(st reflect.StructField) bool {
 	return len(nm) > 0 && unicode.IsUpper(rune(nm[0]))
 }
 
-var errJSONMarshalMap = errors.New("maps are unsupported")
+var errJSONMarshalMap = errors.New("maps are not supported")
 
 var (
 	bytesNull             = []byte("null")
@@ -239,6 +243,10 @@ func jsonFieldName(f reflect.StructField) (string, bool) {
 		head = f.Name
 	}
 	return head, omitEmpty
+}
+
+func nilBytes(b []byte) bool {
+	return len(b) == 0 || bytes.Equal(b, bytesNull)
 }
 
 func allBlankBytes(b []byte) bool {
@@ -285,8 +293,200 @@ func isBlankInterface(v interface{}) bool {
 	}
 }
 
+var errExpectingOpenBrace = errors.New("expecting '{'")
+
+type disfixRepr struct {
+	Disfix string      `json:"_df"`
+	Data   interface{} `json:"_v"`
+}
+
+var blankDisfix DisfixBytes
+
+// parseDisfixAndData helps unravel the disfix and the stored data expected in the form
+// {
+//    "_df": "XXXXXXXXXXXXXXXXX",
+//    "_v":  {}
+// }
+// It then returns
+func parseDisfixAndData(bz []byte) (disfix DisfixBytes, dataBytes []byte, err error) {
+	bz = bytes.TrimSpace(bz)
+	if len(bz) < DisfixBytesLen {
+		return disfix, bz, errors.New("EOF skipping prefix bytes.")
+	}
+	dfr := new(disfixRepr)
+	if err := json.Unmarshal(bz, dfr); err != nil {
+		return disfix, bz, fmt.Errorf("Parsing Disfix and data: %v", err)
+	}
+	hexBytes, err := hex.DecodeString(dfr.Disfix)
+	if err != nil {
+		return disfix, bz, err
+	}
+	if g, w := len(hexBytes), DisfixBytesLen; g != w {
+		return disfix, bz, fmt.Errorf("Disfix length got=%d want=%d", g, w)
+	}
+	copy(disfix[:], hexBytes)
+	if bytes.Equal(disfix[:], blankDisfix[:]) {
+		return disfix, bz, errors.New("expected a non-blank disfix")
+	}
+	dataBytes, err = json.Marshal(dfr.Data)
+	return disfix, dataBytes, err
+}
+
 //----------------------------------------
 // cdc.decodeReflectJSON
 
-func (cdc *Codec) decodeReflectJSON(blob []byte, out interface{}) {
+// CONTRACT: rv.CanAddr() is true.
+func (cdc *Codec) decodeReflectJSON(bz []byte, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
+	if !rv.CanAddr() {
+		panic("rv not addressable")
+	}
+
+	if info.Registered {
+		// Expecting
+		//  {"_df":"<DF>","_v":"data"}
+		if len(bz) < DisfixBytesLen {
+			return errors.New("EOF skipping prefix bytes.")
+		}
+		gotDisfix, bzRest, err := parseDisfixAndData(bz)
+		if err != nil {
+			return err
+		}
+		// Ensure that the concrete type matches the distfix
+		wantDisfix := toDisfix(info.Disamb, info.Prefix)
+		if g, w := gotDisfix[:], wantDisfix[:]; !bytes.Equal(g, w) {
+			return fmt.Errorf("decodeReflectJSON: distfix mismatch got=%X want=%X", g, w)
+		}
+		bz = bzRest
+	}
+
+	// SANITY CHECK
+	if info.Type.Kind() == reflect.Interface && rv.Kind() == reflect.Ptr {
+		panic("should not happen")
+	}
+
+	// Special case for nil
+	if nilBytes(bz) {
+		rv.Set(info.ZeroValue)
+		return nil
+	}
+
+	// Handle pointer types.
+	if rv.Kind() == reflect.Ptr {
+		// Dereference-and-construct pointers all the way.
+		// This works for pointer-pointers.
+		for c := true; c; c = rv.Kind() == reflect.Ptr {
+			if rv.IsNil() {
+				newPtr := reflect.New(rv.Type().Elem())
+				rv.Set(newPtr)
+			}
+			rv = rv.Elem()
+		}
+	}
+
+	switch info.Type.Kind() {
+
+	//----------------------------------------
+	// Complex
+
+	case reflect.Interface:
+		return cdc.decodeReflectJSONInterface(bz, info, rv, opts)
+
+	case reflect.Array, reflect.Slice:
+		return cdc.decodeReflectJSONArrayOrSlice(bz, info, rv, opts)
+
+	case reflect.Struct:
+		return cdc.decodeReflectJSONStruct(bz, info, rv, opts)
+
+	//----------------------------------------
+
+	case reflect.Map: // We explicitly don't support maps
+		return errJSONMarshalMap
+
+	default: // All others
+		return invokeStdlibJSONUnmarshal(bz, info, rv, opts)
+	}
+}
+
+func invokeStdlibJSONUnmarshal(bz []byte, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
+	if !rv.CanAddr() && rv.Kind() != reflect.Ptr {
+		panic("rv not addressable nor pointer")
+	}
+
+	var rrv reflect.Value = rv
+	if rv.Kind() != reflect.Ptr {
+		rrv = reflect.New(rv.Type())
+	}
+	if err := json.Unmarshal(bz, rrv.Interface()); err != nil {
+		return err
+	}
+	rv.Set(rrv.Elem())
+	return nil
+}
+
+func (cdc *Codec) decodeReflectJSONInterface(bz []byte, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
+	if !rv.CanAddr() {
+		panic("rv not addressable")
+	}
+
+	// Special case for nil
+	if nilBytes(bz) {
+		rv.Set(info.ZeroValue)
+		return nil
+	}
+
+	// Concrete reflect value
+	crv := safeElem(rv)
+
+	crv, err := deref(crv, info)
+	if err != nil {
+		return err
+	}
+
+	// Get concrete type info.
+	var cinfo *TypeInfo
+	if !info.Registered {
+		cinfo = info
+	} else {
+		disfix, restBlob, err := parseDisfixAndData(bz)
+		if err != nil {
+			return err
+		}
+		bz = restBlob
+		cinfo, err = cdc.getTypeInfoFromDisfix_rlock(disfix)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	// Construct new concrete type.
+	// NOTE: rv.Set() should succeed because it was validated
+	// already during Register[Interface/Concrete].
+	var rvSet reflect.Value
+	if cinfo.PointerPreferred {
+		cPtrRv := reflect.New(cinfo.Type)
+		crv = cPtrRv.Elem()
+		rvSet = cPtrRv
+	} else {
+		crv = reflect.New(cinfo.Type).Elem()
+		rvSet = crv
+	}
+
+	// Read into crv.
+	if err := cdc.decodeReflectJSON(bz, cinfo, crv, opts); err != nil {
+		return err
+	}
+
+	rv.Set(rvSet)
+	return nil
+}
+
+func (cdc *Codec) decodeReflectJSONArrayOrSlice(bz []byte, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
+	panic("Unimplemented")
+	return nil
+}
+
+func (cdc *Codec) decodeReflectJSONStruct(bz []byte, info *TypeInfo, rv reflect.Value, opts FieldOptions) error {
+	panic("Unimplemented")
+	return nil
 }
--- wire.go
+++ wire.go
@@ -2,7 +2,6 @@ package wire
 
 import (
 	"bytes"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"reflect"
