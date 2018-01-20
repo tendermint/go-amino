90af61ec85415e05c7777cd50ff93ba40d34b1e8
Jae Kwon <jae@tendermint.com>
2018-01-20 01:03:34 -0800

    JSON stub marshal/unmarshal functions for codec (#82)
    * add temporary json stub

--- wire_test.go
+++ wire_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/tendermint/go-wire"
 )
 
-func TestMarshalGlobal(t *testing.T) {
+func TestMarshalBinaryGlobal(t *testing.T) {
 
 	type SimpleStruct struct {
 		String string
@@ -27,6 +27,30 @@ func TestMarshalGlobal(t *testing.T) {
 
 	var s2 SimpleStruct
 	err = wire.UnmarshalBinary(b, &s2)
+	assert.Nil(t, err)
+	assert.Equal(t, s, s2)
+
+}
+
+func TestMarshalJSONGlobal(t *testing.T) {
+
+	type SimpleStruct struct {
+		String string
+		Bytes  []byte
+		Time   time.Time
+	}
+
+	s := SimpleStruct{
+		String: "hello",
+		Bytes:  []byte("goodbye"),
+		Time:   time.Now().UTC().Truncate(time.Millisecond), // strip monotonic and timezone.
+	}
+
+	b, err := wire.MarshalJSON(s)
+	assert.Nil(t, err)
+
+	var s2 SimpleStruct
+	err = wire.UnmarshalJSON(b, &s2)
 	assert.Nil(t, err)
 	assert.Equal(t, s, s2)
 
