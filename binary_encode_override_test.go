ab8d1f29ed28a92f0f70519dfe5c887af4b715ab
Jordan Sexton <jordan@jordansexton.com>
2019-12-17 15:01:08 -0600

    add test

---
+++ binary_encode_override_test.go
@@ -0,0 +1,44 @@
+package amino_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	amino "github.com/tendermint/go-amino"
+	"testing"
+)
+
+type Thing struct {
+	Name string
+}
+
+// func (thing Thing) MarshalAmino() (string, error) {
+// 	return thing.Name, nil
+// }
+//
+// func (thing Thing) UnmarshalAmino(name string) error {
+// 	thing.Name = name
+// 	return nil
+// }
+
+func (thing Thing) MarshalBinary() ([]byte, error) {
+	return []byte(thing.Name), nil
+}
+
+func (thing *Thing) UnmarshalBinary(bz []byte) error {
+	thing.Name = string(bz)
+	return nil
+}
+
+func TestMarshalBinaryOverride(t *testing.T) {
+	var cdc = amino.NewCodec()
+	cdc.RegisterConcrete(&Thing{}, "amino/thing", nil)
+
+	thing1 := Thing{Name: "a"}
+
+	bz, err := cdc.MarshalBinaryBare(thing1)
+	assert.Nil(t, err)
+
+	var thing2 Thing
+	err = cdc.UnmarshalBinaryBare(bz, &thing2)
+	assert.Nil(t, err)
+	assert.Equal(t, thing1, thing2)
+}
