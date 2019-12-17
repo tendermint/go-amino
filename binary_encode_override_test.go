0ac91de6403c093a2a00b5472313c522d0ab3c9c
Jordan Sexton <jordan@jordansexton.com>
2019-12-17 16:41:31 -0600

    add more tests

--- binary_encode_override_test.go
+++ binary_encode_override_test.go
@@ -20,7 +20,7 @@ func (thing *Thing) UnmarshalBinary(bz []byte) error {
 	return nil
 }
 
-func TestMarshalBinaryOverride(t *testing.T) {
+func TestMarshalBinaryOverrideBare(t *testing.T) {
 	var cdc = amino.NewCodec()
 	cdc.RegisterConcrete(&Thing{}, "amino/thing", nil)
 
@@ -28,9 +28,56 @@ func TestMarshalBinaryOverride(t *testing.T) {
 
 	bz, err := cdc.MarshalBinaryBare(thing1)
 	assert.Nil(t, err)
+	assert.Equal(t, bz, []byte{140, 74, 30, 175, 97})
 
 	var thing2 Thing
 	err = cdc.UnmarshalBinaryBare(bz, &thing2)
 	assert.Nil(t, err)
 	assert.Equal(t, thing1, thing2)
+}
+
+func TestMarshalBinaryOverrideLengthPrefixed(t *testing.T) {
+	var cdc = amino.NewCodec()
+	cdc.RegisterConcrete(&Thing{}, "amino/thing", nil)
+
+	thing1 := Thing{Name: "a"}
+
+	bz, err := cdc.MarshalBinaryLengthPrefixed(thing1)
+	assert.Nil(t, err)
+	assert.Equal(t, bz, []byte{5, 140, 74, 30, 175, 97})
+
+	var thing2 Thing
+	err = cdc.UnmarshalBinaryLengthPrefixed(bz, &thing2)
+	assert.Nil(t, err)
+	assert.Equal(t, thing1, thing2)
+}
+
+type Bytes [16]byte
+
+func (bytes Bytes) MarshalBinary() ([]byte, error) {
+	bz := make([]byte, 17)
+	copy(bz[:1], []byte{16})
+	copy(bz[1:], bytes[:])
+	return bz, nil
+}
+
+func (bytes *Bytes) UnmarshalBinary(bz []byte) error {
+	copy(bytes[:], bz[1:])
+	return nil
+}
+
+func TestMarshalBinaryOverrideBytes(t *testing.T) {
+	var cdc = amino.NewCodec()
+	cdc.RegisterConcrete(&Bytes{}, "amino/bytes", nil)
+
+	bytes1 := Bytes{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}
+
+	bz, err := cdc.MarshalBinaryBare(bytes1)
+	assert.Nil(t, err)
+	assert.Equal(t, bz, []byte{207, 109, 94, 111, 16,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1})
+
+	var bytes2 Bytes
+	err = cdc.UnmarshalBinaryBare(bz, &bytes2)
+	assert.Nil(t, err)
+	assert.Equal(t, bytes1, bytes2)
 }
