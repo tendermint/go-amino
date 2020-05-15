39f3450d87c6f3f0cc3f4a69c2ab5dd195773371
Jae Kwon <jae@tendermint.com>
2020-05-14 17:56:19 -0700

    scan a file and print relevant structs

---
+++ genproto/example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// amino
+type StructA struct {
+	fieldA int
+	fieldB int
+	FieldC int
+	FieldD uint32
+}
+
+// amino
+type StructB struct {
+	fieldA int
+	fieldB int
+	FieldC int
+	FieldD uint32
+}
+
+func main() {
+	fmt.Println("dontcare")
+}
---
+++ genproto/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
+)
+
+type aminoType struct {
+	directive *struct{} // amino directive is initially binary, no options
+	name      string
+	typeExpr  ast.Expr
+}
+
+func main() {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "example/main.go", nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Inspect(node, func(n ast.Node) bool {
+
+		switch n.(type) {
+		case *ast.GenDecl:
+			decl := n.(*ast.GenDecl)
+			comments := decl.Doc
+
+			// currenty just a bool, but in the future may be extended with options.
+			var declDirective *struct{} // at the group level?
+			var typeDirective *struct{} // at the line level?
+			var aminoTypes []aminoType
+
+			// look for amino directives in comment for GenDecl.
+			if comments != nil {
+				for _, comment := range comments.List {
+					if strings.HasPrefix(comment.Text, "// amino") {
+						declDirective = &struct{}{}
+						break // TODO: instead of breaking, merge decl directives
+					}
+				}
+			}
+
+			// look for amino directives in comment for TypeSpec.
+			for _, spec := range decl.Specs {
+				typeDirective = nil
+				switch spec := spec.(type) {
+				case *ast.TypeSpec:
+					if comments := spec.Doc; comments != nil {
+						for _, comment := range comments.List {
+							if strings.HasPrefix(comment.Text, "// amino") {
+								typeDirective = &struct{}{}
+								break // TODO: instead of breaking, merge type directives
+							}
+						}
+					}
+					if comments := spec.Comment; comments != nil {
+						for _, comment := range comments.List {
+							if strings.HasPrefix(comment.Text, "// amino") {
+								typeDirective = &struct{}{}
+								break // TODO: instead of breaking, merge type directives
+							}
+						}
+					}
+
+					// We found a type spec to apply amino directives to.
+					directive := mergeDirectives(declDirective, typeDirective)
+					if directive != nil {
+						switch stype := spec.Type.(type) {
+						case *ast.StructType:
+							aminoTypes = append(aminoTypes, aminoType{
+								directive: directive,
+								name:      spec.Name.Name,
+								typeExpr:  spec.Type,
+							})
+						default:
+							panic(fmt.Sprintf("amino directive unsupported for %v", stype))
+						}
+					}
+				}
+			}
+
+			// for each amino type, run directives.
+			for _, aminoType := range aminoTypes {
+				fmt.Println("Generating proto schema for:", aminoType.name)
+				switch atype := aminoType.typeExpr.(type) {
+				case *ast.StructType:
+					// do something
+				default:
+					panic(fmt.Sprintf("amino directive unsupported for %v", atype))
+				}
+			}
+
+		default:
+
+			// nothing
+		}
+
+		return true
+		/*
+			ret, ok := n.(*ast.ReturnStmt)
+			if ok {
+				fmt.Printf("return statement found on line %d:\n\t", fset.Position(ret.Pos()).Line)
+				printer.Fprint(os.Stdout, fset, ret)
+				return true
+			}
+		*/
+		return true
+	})
+}
+
+// TODO actually define directive options and merge them.
+func mergeDirectives(a *struct{}, b *struct{}) *struct{} {
+	if a != nil {
+		return a
+	}
+	return b
+}
